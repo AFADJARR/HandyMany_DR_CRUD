3812be4314dbc24e8481815efff298f45a796055
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2026-04-10 10:23:57 +0700

    menambhakna fungi fi tombol menambahkan data

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.txtAlamat = new System.Windows.Forms.TextBox();
             this.txtKodeProdi = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btninsert = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -175,14 +175,15 @@
             this.button1.Text = "Mengubah Data";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btninsert
             // 
-            this.button2.Location = new System.Drawing.Point(497, 114);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 26);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Menambah Data";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btninsert.Location = new System.Drawing.Point(497, 114);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(135, 26);
+            this.btninsert.TabIndex = 17;
+            this.btninsert.Text = "Menambah Data";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
             // 
             // button3
             // 
@@ -199,7 +200,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btninsert);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtKodeProdi);
             this.Controls.Add(this.txtAlamat);
@@ -239,7 +240,7 @@
         private System.Windows.Forms.TextBox txtAlamat;
         private System.Windows.Forms.TextBox txtKodeProdi;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btninsert;
         private System.Windows.Forms.Button button3;
     }
 }
